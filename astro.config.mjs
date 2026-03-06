75ff893e62a1b37c3e7b18a82c363aaaf9f8e68d
David <gilf6095@gmail.com>
2026-03-06 19:39:00 +0100

    chore: initial astro setup, tailwind config and .prettierrc

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import compress from 'astro-compress';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [compress()]
+});
